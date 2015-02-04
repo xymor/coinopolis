bb8361e7f712ae9532eb8cdf2548dbe64a4d6078
Raphael Miranda <raphael.miranda@ideais.com.br>
2015-02-04 19:29:52 -0200

    corrigindo HC

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -13,6 +13,6 @@ class IndexController < ApplicationController
 		q = params[:from] ? params : { :from => "USD", :to => "BRL",  :date_start => "2014-01-01", :date_end => "2015-01-01" }
 		@quotes = Jsonrates::Client.historical(q)
 
-		@quotes.inject({}) { |h,v| h.merge( v[1]["utctime"] => v[1]["rate"] ) }.to_json
+		@quotes.collect{ |v| v[1]["rate"] }.to_json
 	end	
 end
