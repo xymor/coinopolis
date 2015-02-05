bfdacff9f427e26b9aef5d18202e5ce20caecaf1
xymor <raphaelmiranda@gmail.com>
2015-02-05 09:48:34 -0200

    :green_heart: test improvement

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -16,14 +16,14 @@ RSpec.describe IndexController, :type => :controller do
 
       q = [:from => "USD", 
             :to => "BRL", 
-            :date_start => "2014-01-01", 
+            :date_start => "2014-01-01",
             :date_end => "2015-01-01"]
 
       out = [1.4,2.3,3.2]      
 
       allow(Jsonrates::Client).to receive(:historical).and_return(out)
 
-      controller.send(:get_data_points?).should eql([1.4,2.3,3.2])
+      controller.send(:get_data_points?).should eql(out)
 
     end
   end  
