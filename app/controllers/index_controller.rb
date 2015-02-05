e5f02c34c6248fd9a3e30d269c1dedbd3f811ad2
xymor <raphaelmiranda@gmail.com>
2015-02-04 22:25:39 -0200

    done

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,17 +2,22 @@ class IndexController < ApplicationController
 
 	def index
 		
+		unless params[:to]
+			return redirect_to root_path(:from => "USD", 
+								  :to => "BRL", 
+								  :date_start => "2014-01-01", 
+								  :date_end => "2015-01-01") 
+		end
+
 		@data_points = get_data_points(params)
 
-		@msg = 'testttt'
 		render :action => "index"
 	end	
 
 	private
-	def get_data_points(params)
-		q = params[:from] ? params : { :from => "USD", :to => "BRL",  :date_start => "2014-01-01", :date_end => "2015-01-01" }
+	def get_data_points(q)
 		@quotes = Jsonrates::Client.historical(q)
 
-		@quotes.collect{ |v| v[1]["rate"] }.to_json
+		@quotes.collect{ |v| v[1]["rate"].to_f }
 	end	
 end
